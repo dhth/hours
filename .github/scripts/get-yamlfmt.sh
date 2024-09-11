2235f043d69160338963f14b64e517ef10293132
Dhruv Thakur <dhruvt93@gmail.com>
2024-09-11 18:48:49 +0200

    ci: use correct architecture name

--- .github/scripts/get-yamlfmt.sh
+++ .github/scripts/get-yamlfmt.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <os> <arch> <version>"
-    echo "eg: $0 Linux amd64 0.13.0"
+    echo "eg: $0 Linux x86_64 0.13.0"
     exit 1
 fi
 
@@ -12,12 +12,24 @@ OS="$1"
 ARCH="$2"
 VERSION="$3"
 
+cwd=$(pwd)
+
+temp_dir=$(mktemp -d)
+if [ ! -e ${temp_dir} ]; then
+    echo "Failed to create temporary directory."
+    exit 1
+fi
+
+cd $temp_dir
+
 curl -sSLO "https://github.com/google/yamlfmt/releases/download/v${VERSION}/yamlfmt_${VERSION}_${OS}_${ARCH}.tar.gz"
 curl -sSLO "https://github.com/google/yamlfmt/releases/download/v${VERSION}/checksums.txt"
 
-ls -1
-cat checksums.txt
-
 sha256sum --ignore-missing -c checksums.txt
 
-tar -xzf "yamlfmt_${VERSION}_${OS}_${ARCH}.tar.gz"
+tar -xzf "yamlfmt_${VERSION}_${OS}_${ARCH}.tar.gz" -C ${temp_dir}/
+cd $cwd
+
+cp "${temp_dir}/yamlfmt" .
+
+rm -r ${temp_dir}
