480434f7ff828bf16f2942cfd0a720978eb71d11
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-02-14 19:51:45 +0100

    use go-lint composite action

--- internal/ui/theme/builtin.go
+++ internal/ui/theme/builtin.go
@@ -17,7 +17,8 @@ type builtInThemePalette struct {
 }
 
 func getBuiltInTheme(palette builtInThemePalette) Theme {
-	taskColors := []string{
+	taskColors := make([]string, 0, 8+len(palette.tasks))
+	taskColors = append(taskColors,
 		palette.primary,
 		palette.secondary,
 		palette.tertiary,
@@ -26,7 +27,7 @@ func getBuiltInTheme(palette builtInThemePalette) Theme {
 		palette.info,
 		palette.warn,
 		palette.error,
-	}
+	)
 	taskColors = append(taskColors, palette.tasks...)
 
 	return Theme{
