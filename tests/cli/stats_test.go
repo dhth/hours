8be273a8f64d5896834fd1c11da7991a2999057e
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-02-22 23:41:56 +0100

    add tests for special date ranges

--- tests/cli/stats_test.go
+++ tests/cli/stats_test.go
@@ -10,14 +10,19 @@ import (
 
 func TestStats(t *testing.T) {
 	fx := NewFixture(t, testBinaryPath)
+	now := time.Date(2025, time.October, 24, 12, 0, 0, 0, time.UTC)
 
-	_, err := fx.RunGen(42, time.Date(2025, time.October, 24, 12, 0, 0, 0, time.UTC))
+	_, err := fx.RunGen(42, now)
 	require.NoError(t, err)
 
 	testCases := []struct {
 		name   string
 		period string
 	}{
+		{name: "today", period: "today"},
+		{name: "yest", period: "yest"},
+		{name: "3d", period: "3d"},
+		{name: "week", period: "week"},
 		{name: "date", period: "2025/10/24"},
 		{name: "date range", period: "2025/10/20...2025/10/24"},
 		{name: "all", period: "all"},
@@ -30,6 +35,7 @@ func TestStats(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			cmd := NewCmd([]string{"stats", "--plain"})
 			cmd.AddArgs(tc.period)
+			cmd.SetEnv("HOURS_NOW", now.Format(time.RFC3339))
 			cmd.UseDB()
 
 			result, runErr := fx.RunCmd(cmd)
